9fe1db1f41e12b7e56c86a90667dcce3b380bf26
Andrii_Cherepakha <Andrey.Cherepakha@gmail.com>
2017-10-30 15:54:40 +0200

    Add get screenshot on TearDown example

--- src/Example/Tests/Class1.cs
+++ src/Example/Tests/Class1.cs
@@ -1,10 +1,7 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using ReportPortal.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using LogLevel = ReportPortal.Client.Models.LogLevel;
 
 namespace Example.Tests
 {
@@ -28,6 +25,32 @@ namespace Example.Tests
         public void Test2()
         {
             Bridge.LogMessage(ReportPortal.Client.Models.LogLevel.Trace, "class1 test2 log message");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Equals(ResultState.Failure) ||
+                TestContext.CurrentContext.Result.Outcome.Equals(ResultState.Error) ||
+                TestContext.CurrentContext.Result.Outcome.Equals(ResultState.SetUpError) ||
+                TestContext.CurrentContext.Result.Outcome.Equals(ResultState.SetUpFailure))
+            {
+                string base64 = GetScreenshotAsBase64EncodedString();
+
+                if (base64 != null)
+                {
+                    Bridge.LogMessage(ReportPortal.Client.Models.LogLevel.Error, "Screen shot on failure {rp#base64#image/png#" + base64 + "}");
+                }
+            }
+        }
+
+        private string GetScreenshotAsBase64EncodedString()
+        {
+            // assume that screenshot is taken with Selenium WebDriver with commented code.
+            // where WebDriver.Instance is an instanse of IWebDriver
+
+            // ((ITakesScreenshot) WebDriver.Instance).GetScreenshot().AsBase64EncodedString;
+            return null;
         }
     }
 }
