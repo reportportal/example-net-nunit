5a81958e6c17a818756b313aa78a292f90ee572b
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2018-08-29 18:19:08 +0300

    Added ignored and failed suites

---
+++ src/Example/Tests/FailedClass.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using ReportPortal.Shared;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Tests
+{
+    [Category("Category 1"), Category("Category 2")]
+    [TestFixture]
+    [Description("This suite throws an exception in OneTimeSetup")]
+    public class FailedClass
+    {
+        [OneTimeSetUp]
+        public void Init()
+        {
+            throw new Exception("Here is OneTimeSetup exception.");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+
+        [Test]
+        public void Test2()
+        {
+
+        }
+    }
+}
---
+++ src/Example/Tests/IgnoredClass.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using ReportPortal.Shared;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Tests
+{
+    [Category("Category 1"), Category("Category 2")]
+    [TestFixture, Ignore("Ignore reason for suite")]
+    [Description("All tests in this suite should be skipped.")]
+    public class IgnoredClass
+    {
+        [Test]
+        public void Test1()
+        {
+            
+        }
+
+        [Test]
+        public void Test2()
+        {
+
+        }
+    }
+}
