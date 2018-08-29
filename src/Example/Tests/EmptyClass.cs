833f65c7ce8e935f553ca602ab02607eba36463f
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2018-08-29 18:25:01 +0300

    Added empty suite

---
+++ src/Example/Tests/EmptyClass.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using ReportPortal.Shared;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Tests
+{
+    [Category("Category 1"), Category("Category 2")]
+    [TestFixture]
+    [Description("Empty suite")]
+    public class EmptyClass
+    {
+
+    }
+}
