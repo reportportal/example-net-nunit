7b587e78aed0a6d61a145fd782f743c9eb2c05ec
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-02-07 14:34:34 +0300

    3k tests with 50 logs

--- src/Example/Tests/Class2.cs
+++ src/Example/Tests/Class2.cs
@@ -41,13 +41,13 @@ namespace Example.Tests
         [Test, Description("My static description, and this test was executed on {MachineName} machine.")]
         public void Test4()
         {
-            
+
         }
 
         [Test, Ignore("My ignore reason")]
         public void IgnoredTest()
         {
-            
+
         }
 
         [Test]
@@ -58,16 +58,22 @@ namespace Example.Tests
         }
 
         [Test, TestCaseSource(typeof(Class2), nameof(Source))]
-        public void TestWithTestCaseSource(int i)
+        public void TestWithTestCaseSource(int k)
         {
+            //var filePath = TestContext.CurrentContext.TestDirectory + "\\dog.png";
+            //Bridge.LogMessage(ReportPortal.Client.Models.LogLevel.Info, "my dog {rp#file#" + filePath + "}");
 
+            for (int i = 0; i < 50; i++)
+            {
+                Bridge.LogMessage(ReportPortal.Client.Models.LogLevel.Info, $"Log message {i}");
+            }
         }
 
         private static IEnumerable Source
         {
             get
             {
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < 3000; i++)
                 {
                     yield return new TestCaseData(i);
                 }
