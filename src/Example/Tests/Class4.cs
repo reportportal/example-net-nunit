70bffaef604cbce15cb79758d9aac4bf3fd7aa88
Maksym Grytsov <goblinmaks@gmail.com>
2020-03-17 17:36:44 +0200

    Add Test for TestCase attribute

---
+++ src/Example/Tests/Class4.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Example.Tests
+{
+    [Parallelizable]
+    
+    [Description("It is Description of Fixture from Class4")]
+    [TestFixture(TestName = "It is TestName of TestFixture")]
+    public class Class4
+    {
+
+        [TestCase]
+        public void Test1()
+        {
+            ReportPortal.Shared.Log.Info("TestCase with Empty [TestCase] attribute");
+        }
+
+        [Description("It is description of TestCase")]
+        [TestCase]
+        public void Test2()
+        {
+            ReportPortal.Shared.Log.Info("TestCase with Description for [TestCase] attribute");
+        }
+
+        [TestCase(TestName = "It is TestName of TestCase")]
+        public void Test3()
+        {
+            ReportPortal.Shared.Log.Info("TestCase with TestName in [TestCase] attribute");
+        }
+    }
+}
